6df8e2019f745a1bcf23c7c9b86a6131758675a5
Michael Conway <mconway@indeed.com>
2023-03-13 10:06:13 +0000

    fix(deselect): deselect selected, fix typo maxheight to height

--- src/Components/BookItem.js
+++ src/Components/BookItem.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const containerStyle = { display: 'flex', maxHeight: '100px', gap: '2rem', border: '1px #555555 solid' }
+const containerStyle = { display: 'flex', height: '100px', gap: '2rem', border: '1px #555555 solid' }
 const imgContainerStyle = { maxWidth: '50px', maxHeight: '75px' };
 const responsiveImageStyle = { objectFit: 'contain', maxWidth: '100%' };
 const detailsStyle = { display: 'flex', flexDirection: 'column' }
 
 const BookItem = ({ book, onItemSelected }) => {
 
+    const itemRef = useRef();
+
+    const unCheck = () => {
+        if (itemRef.current.checked) {
+            itemRef.current.checked = false;
+        }
+    }
+
     return (
         <div data-testid="book-item" style={containerStyle}>
-            <div><input type="checkbox" onChange={onItemSelected} /></div>
+            <div><input ref={itemRef} type="checkbox" onChange={(e) => onItemSelected(e, unCheck)} /></div>
             <div style={imgContainerStyle}>{
                 <img style={responsiveImageStyle} alt={`${book.title} cover`} src={book?.formats && book?.formats['image/jpeg']} />
             }</div>
--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -4,12 +4,13 @@ import Header from './Header'
 import { useBooksList } from '../Hooks/useBookList';
 import { useState } from 'react';
 
-const LoadingStyle = { width: '5rem', padding: '1rem', position: 'absolute', bottom: '15%', left: '47%', zIndex: 99, backgroundColor: '#000000', color: '#ffffff' }
-const scroller = { height: '500px', overflow: 'scroll' };
+const LoadingStyle = { width: '5rem', padding: '1rem', position: 'absolute', bottom: '20%', left: '47%', zIndex: 99, backgroundColor: '#000000', color: '#ffffff' }
+const scroller = { height: '500px', overflow: 'auto' };
 
 const Booklist = () => {
     const { books, loading, error, next, retrievedCount, listBooks } = useBooksList();
     const [countSelected, setCountSelected] = useState(0)
+    const [deselectList, setDeselectList] = useState([]);
     const bookListRef = useRef();
     const handleScroll = () => {
         const isAtBottom = Math.abs(bookListRef?.current.scrollHeight - bookListRef?.current.clientHeight - bookListRef?.current.scrollTop) < 1;
@@ -23,13 +24,17 @@ const Booklist = () => {
     }, [listBooks])
 
 
-    const handleItemChecked = useCallback((e) => {
+    const handleItemChecked = useCallback((e, fn) => {
+        setDeselectList((prev) => [...prev, fn])
         setCountSelected((prev) => e.target.checked ? prev + 1 : prev - 1)
-    }, [setCountSelected]);
+    }, [setCountSelected, setDeselectList]);
 
     const resetAllSelected = useCallback(() => {
+        deselectList.forEach((fn) => {
+            fn();
+        })
         setCountSelected(0)
-    }, [setCountSelected])
+    }, [setCountSelected, deselectList])
 
     const renderBookItems = useMemo(() => {
         return books?.map((book) => (
